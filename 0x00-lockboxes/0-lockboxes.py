2df084c16424dcba6910416e86aaadaaf226cfbf
Rachel Lewis <RLewis11769@gmail.com>
2021-11-03 02:58:47 +0000

    Add solution to lockboxes task

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def canUnlockAll(boxes):
+    """ Returns T/F if all boxes can be unlocked """
+    key_list = [0]
+    # box is index of each box
+    for box in range(len(boxes)):
+        # test_key is every potential key whether reachable or not
+        for test_key in range(box, len(boxes)):
+            if test_key in key_list:
+                # key is reachable key as found in box where key is in key_list
+                for key in boxes[test_key]:
+                    if key not in key_list:
+                        [key_list.append(key)]
+    # Return if key_list contains key to all locked boxes
+    for lock in range(len(boxes)):
+        if lock not in key_list:
+            return False
+    return True
