73c9ffb0e9028f06f26d0eed01786acc521366fc
Rachel Lewis <RLewis11769@gmail.com>
2021-11-05 22:43:41 +0000

    Whatever - revert to old way

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,19 +4,25 @@
 
 def canUnlockAll(boxes):
     """ Returns True if all boxes have reachable key """
-    # Create a list of key_list with default box being 0
+    if not boxes:
+        return False
+    # Initializes list of unlocked boxes with first box
     key_list = [0]
-    # box is index of box
-    # list is list of all keys inside each box
-    for box, list in enumerate(boxes):
-        # idx starts off at current box for each loop so not repeatedly checking
-        for idx in range(box, len(boxes)):
-            if idx in key_list:
-                # Same sort of appending as before
-                [key_list.append(key) for key in boxes[idx] if key not in key_list]
-        # Attempt to open box that met above else condition
-        if box in key_list:
-            [key_list.append(key) for key in list if key not in key_list]
-        else:
+    # idx is index of each box
+    for idx in range(len(boxes)):
+        # test_key is every potential key whether reachable or not
+        for test_key in range(idx, len(boxes)):
+            if test_key in key_list:
+                # key is reachable key as found in box where key is in key_list
+                for key in boxes[test_key]:
+                    # if already in list, break from loop to save memory
+                    if key in key_list:
+                        break
+                    # if not in list but needs to be, add to list
+                    if key not in key_list:
+                        key_list.append(key)
+    # Return if key_list contains key to all locked boxes
+    for box_key in range(len(boxes)):
+        if box_key not in key_list:
             return False
     return True
