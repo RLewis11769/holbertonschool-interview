506eae3c3c2efe0a75e681acbb5e211c961b39bd
Rachel Lewis <73726775+RLewis11769@users.noreply.github.com>
2021-11-05 13:21:14 -0500

    Try different way with recursive function

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,21 +4,17 @@
 
 def canUnlockAll(boxes):
     """ Returns True if all boxes have reachable key """
-    if not boxes:
-        return False
-    # Initializes list of unlocked boxes with first box
-    key_list = [0]
-    # box is index of each box
-    for box in range(len(boxes)):
-        # test_key is every potential key whether reachable or not
-        for test_key in range(box, len(boxes)):
-            if test_key in key_list:
-                # key is reachable key as found in box where key is in key_list
-                for key in boxes[test_key]:
-                    if key not in key_list:
-                        key_list.append(key)
-    # Return if key_list contains key to all locked boxes
-    for box_key in range(len(boxes)):
-        if box_key not in key_list:
-            return False
-    return True
+    key_list = set()
+
+    def test_box(box):
+        """ Add index of this box to list """
+        key_list.add(box)
+        for key in boxes[box]:
+            if key not in key_list:
+                test_box(key)
+
+    # Start with first box
+    test_box(0)
+
+    # If all boxes are in set, return True
+    return len(key_list) == len(boxes)
