4665a35c4e592aa2c5e5b1e8edc3c33bccf7cfc7
Rachel Lewis <RLewis11769@gmail.com>
2021-12-03 16:47:01 +0000

    Fix prototype in header

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -4,6 +4,6 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int unstable_grid(int grid1[3][3], int flags[3][3]);
 void topple(int grid1[3][3], int row, int col);
-void print_grid(int grid[3][3]);
+static void print_grid(int grid[3][3]);
 
 #endif
