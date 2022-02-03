b12177db29f571b50e0d50a769b3d0e20239c2e4
Rachel Lewis <RLewis11769@gmail.com>
2022-02-03 15:01:04 -0600

    Not sliding with trailing zeroes but not bad right slide

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -18,29 +18,33 @@ int slide_line(int *line, size_t size, int direction)
 
 	for (x = 0, y = 0; x < size; x++)
 	{
-		/* Skip 0s */
 		if (!line[x])
 			continue;
-		/* Otherwise copy current source array into destination array */
+		/* Copy everything except zeroes into destination array */
 		array[y++] = line[x];
 	}
 
 	/* Fill in remainder of array with zeros */
-	for (; y < size; y++)
+	for (; y < size - 1; y++)
 		array[y] = 0;
+
+	/* Copy array back into orig array */
+	for (x = 0; x < size - 1; x++)
+		line[x] = array[x];
 
 	if (direction == SLIDE_LEFT)
 	{
 		for (x = 0; x < size - 1; x++)
-		{
-			if (array[x] == array[x + 1])
-				left_shift(array, size, x);
-		}
+			if (line[x] == line[x + 1])
+				left_shift(line, size, x);
 	}
 
-	/* Copy array back into orig array */
-	for (x = 0; x < size; x++)
-		line[x] = array[x];
+	if (direction == SLIDE_RIGHT)
+	{
+		for (x = size - 1; x > 0; x--)
+			if (line[x] == line[x - 1])
+				right_shift(line, size, x);
+	}
 	return (1);
 }
 
@@ -57,5 +61,23 @@ void left_shift(int *line, size_t size, size_t x)
 	/* Shift all other elements to the left */
 	for (x = x + 1; x < size - 1; x++)
 		line[x] = line[x + 1];
+	/* Set last element to 0 */
 	line[size - 1] = 0;
+}
+
+/**
+ * right_shift - Shifts an array to the right by one
+ * @line: Pointer to the array to shift
+ * @size: Number of elements in array
+ * @x: Index of the element to shift
+ */
+void right_shift(int *line, size_t size, size_t x)
+{
+	/* Double value of current element */
+	line[x] *= 2;
+	/* Shift all other elements to the right */
+	for (x = x - 1; x > 0; x--)
+		line[x] = line[x - 1];
+	/* Set first element to 0 */
+	line[0] = 0;
 }
